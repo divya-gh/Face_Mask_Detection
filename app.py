ae81be12bd5022e01a6d37b72908efcf83b0764f
eukestad <69314608+eukestad@users.noreply.github.com>
2021-06-12 10:34:51 -0500

    "saving progress"

--- app.py
+++ app.py
@@ -19,19 +19,21 @@ def index():
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route("/capture_img")
+@app.route("/get_image")
 def capture():
-    new_capture = capture_img()
+    img = capture_img()
 
-    result = test_img(new_capture)
+    # img = cv2.imread('./Resources/Test/85-with-mask.jpg')
+
+    result = test_img(img)
 
     return render_template('index2.html', result= result)
 
-# @app.route("/get_image/<img>")
-# def get_predicted_image(img):
+@app.route("/get_image/<img>")
+def get_predicted_image(img):
     
     
-#     return redirect("/", code=302)
+    return redirect("/", code=302)
 
 
 
--- facemask_test.py
+++ facemask_test.py
@@ -1,8 +1,5 @@
 import cv2
-import pickle
 import matplotlib.pyplot as plt
-import pandas as pd
-import time
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
@@ -14,18 +11,13 @@ from tensorflow.keras.layers.experimental.preprocessing import RandomFlip,Random
 
 def test_img(img):
 
-    our_model = pickle.load(open('./Saved_Model/CNN_Model.h5','rb'))
-    capture_score = our_model.score()
+    our_model = keras.models.load_model('./Saved_Model/keras_model.hdf5')
 
     face_model = cv2.CascadeClassifier('./Resources/haarcascade_frontalface_default.xml')
-
-    img = cv2.imread('./Resources/Test/85-with-mask.jpg')
 
     img = cv2.cvtColor(img, cv2.IMREAD_GRAYSCALE)
 
     faces = face_model.detectMultiScale(img,scaleFactor=1.1, minNeighbors=4)
-
-    #img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
     mask_label = {0:'Mask Found',1:'No Mask Found'}
     color_label = {0:(0,255,0),1:(255,0,0)}
@@ -35,12 +27,12 @@ def test_img(img):
     # if more than one face found,
     if len(faces)>=1:
         label = [0 for i in range(len(faces))]
-        for i in range(len(faces)-1):
-            for j in range(i+1, len(faces)):
-                dist = distance.euclidean(faces[i][:2],faces[j][:2])
-                if dist<MIN_DISTANCE:
-                    label[i] = 1
-                    label[j] = 1
+        # for i in range(len(faces)-1):
+        #     for j in range(i+1, len(faces)):
+        #         dist = distance.euclidean(faces[i][:2],faces[j][:2])
+        #         if dist<MIN_DISTANCE:
+        #             label[i] = 1
+        #             label[j] = 1
         new_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR) #colored output image
         # For every face found, 
         for i in range(len(faces)):
@@ -50,8 +42,8 @@ def test_img(img):
             crop = np.reshape(crop,[1,128,128,3])/255.0        
             mask_result = our_model.predict(crop)
             
-            cv2.putText(new_img,mask_label[round(mask_result[0][0])],(x, y-3), cv2.FONT_HERSHEY_SIMPLEX,0.5,color_label[label[i]],2)
-            cv2.rectangle(new_img,(x,y),(x+w,y+h),color_label[label[i]],1)
+            cv2.putText(new_img,mask_label[round(mask_result[0][0])],(x, y-3), cv2.FONT_HERSHEY_SIMPLEX,0.5,color_label[round(mask_result[0][0])],2)
+            cv2.rectangle(new_img,(x,y),(x+w,y+h),color_label[round(mask_result[0][0])],1)
             
         plt.figure(figsize=(10,10))
         return plt.imshow(new_img)
